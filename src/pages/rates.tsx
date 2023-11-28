b51e6f7d55efc90a7410cd153489f4531a49b8b6
DelaBandara <120809445+DelaBandara@users.noreply.github.com>
2023-11-28 19:45:05 +0530

    Update rates.tsx

--- src/pages/rates.tsx
+++ src/pages/rates.tsx
@@ -1,22 +1,70 @@
 import React, { useState, useEffect } from 'react';
 
-const Rates = () => {
+const Rates: React.FC = () => {
+  const [exchangeRates, setExchangeRates] = useState<any>(null); // You might want to replace 'any' with a more specific type
 
+  useEffect(() => {
+    const fetchExchangeRates = async () => {
+      try {
+        const apiKey = '0f9cc0bf1c28690c09d2590e'; // Replace with your Open Exchange Rates API key
+        const response = await fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`);
+        const data = await response.json();
+        setExchangeRates(data.conversion_rates); // Corrected line
+      } catch (error) {
+        console.error('Error fetching exchange rates:', error);
+      }
+    };
 
-  
-    
+    fetchExchangeRates();
+  }, []);
+
+  const renderPrices = (currency: string) => {
+    if (!exchangeRates) {
+      return (
+        <div key={currency}>
+          <p>{currency}</p>
+          <p>Buying Price: N/A</p>
+          <p>Selling Price: N/A</p>
+        </div>
+      );
+    }
+
+    const buyingPrice = exchangeRates[currency];
+    const sellingPrice = 1 /buyingPrice;
+
+    if (isNaN(buyingPrice) || isNaN(sellingPrice)) {
+      return (
+        <div key={currency}>
+          <p>{currency}</p>
+          <p>Invalid exchange rates</p>
+        </div>
+      );
+    }
+
+    return (
+      <div key={currency}>
+        <p>{currency}</p>
+        <p>Buying Price: {buyingPrice}</p>
+        <p>Selling Price: {sellingPrice}</p>
+      </div>
+    );
+  };
 
   return (
     <div>
-       <p>Document content goes here...</p> 
-       
-       <iframe src = "https://www.x-rates.com/table/?from=USD&amount=1" width = "555" height = "200"> 
-          Sorry your browser does not support inline frames. 
-       </iframe> 
-        
-       <p>Document content also go here...</p>
+
+      {exchangeRates && (
+        <>
+          {renderPrices('LKR')} {/* Example for India */}
+          {renderPrices('USD')} {/* Example for United States */}
+          {/* Add more countries as needed */}
+        </>
+      )}
     </div>
   );
+
+  
+
 };
 
 export default Rates;
