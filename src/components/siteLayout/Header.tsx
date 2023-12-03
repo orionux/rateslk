054ccd813382b08aad7960690aff58c12150e2e8
DelaBandara <120809445+DelaBandara@users.noreply.github.com>
2023-12-03 22:04:17 +0530

    mobile drawer/sidebar

--- src/components/siteLayout/Header.tsx
+++ src/components/siteLayout/Header.tsx
@@ -3,8 +3,22 @@ import Link from 'next/link'
 import React from 'react'
 import styles from '@/styles/Home.module.css'
 
+import { useState } from 'react';
+import { IoMdClose, IoIosMenu } from 'react-icons/io';
 
 const Header = () => {
+
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+
   return (
     <>
       <div className="d-flex w-100 px-2 px-lg-5 py-2 justify-content-center align-items-center">
@@ -13,9 +27,40 @@ const Header = () => {
             <Link className="navbar-brand" href="#">
               <Image src={"/layoutimg/Logo.png"} width={120} height={80} alt={''} className='img-fluid' />
             </Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" onClick={toggleSidebar}>
               <span className="navbar-toggler-icon"></span>
             </button>
+            
+      {/*<div className={`${styles.toggButton} d-lg-none d-flex justify-content-center`} onClick={toggleSidebar}>
+        <IoIosMenu className='border border-grey' />
+  </div>*/}
+
+      
+      <div className={`${styles.sidebar} ${isSidebarOpen ? styles.open : ''}`}>
+        <div className={`${styles.logoCloseContainer} d-flex justify-content-between align-items-center p-4`}>
+          <div className={`${styles.logo}`}>
+            <Image src='/icons/sidebarlogo.png' width={107} height={28} alt='Rates.lk' />
+          </div>
+          
+          {isSidebarOpen && (
+            <div className={`${styles.closeButton} d-lg-none`} onClick={closeSidebar}>
+              <IoMdClose className='display-6' />
+            </div>
+          )}
+        </div>
+
+        <div className={`${styles.menu} ps-4 text-start`}>
+          <a href=''className='text-decoration-none text-dark'>Daily Rates</a>
+          <hr />
+          <a href='' className='text-decoration-none text-dark'>Contact Us</a>
+          <hr />
+          <a href='' className='text-decoration-none text-dark'>Get Help</a>
+          <hr />
+          <a href='' className='text-decoration-none text-dark'>What`s New</a>
+          <hr />
+        </div>
+      </div>
+    
           </div>
           <div className="d-flex">
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
