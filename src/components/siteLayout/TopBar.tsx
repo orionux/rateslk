e2a7b2e9444b4e804d8a71a5ced7819141fe04b2
tj <thinupavani@gmail.com>
2023-12-09 14:27:23 +0530

    update

--- src/components/siteLayout/TopBar.tsx
+++ src/components/siteLayout/TopBar.tsx
@@ -3,8 +3,8 @@ import styles from '@/styles/Home.module.css'
 
 const TopBar = () => {
   return (
-    <div className={`d-flex  text-center w-100 justify-content-center ${styles.topbarWrapper}`}>
-      <p className='mb-0 py-2'>Buy & Sell, and 100+ cryptos in minutes. Register now & get ₹100 cashback on your first trade of ₹500 or more!</p>
+    <div className={`d-none d-lg-flex  text-center w-100 justify-content-center ${styles.topbarWrapper}`}>
+      <p className='mb-0 py-2 px-2'>Buy & Sell, and 100+ cryptos in minutes. Register now & get ₹100 cashback on your first trade of ₹500 or more!</p>
     </div>
   )
 }
