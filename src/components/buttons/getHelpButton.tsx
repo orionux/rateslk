02970de8629a8ddb98f1e0da7a530da6a5730fd4
tj <thinupavani@gmail.com>
2023-12-23 22:29:04 +0530

    update

--- src/components/buttons/getHelpButton.tsx
+++ src/components/buttons/getHelpButton.tsx
@@ -10,7 +10,7 @@ const GetHelpButton = () => {
     return (
         <>
             <Link style={{textDecoration: "none"}} href="https://api.whatsapp.com/send?phone=94717697736">
-            <button className={`${styles.getHelpButton} p-3 d-none d-md-block`}>
+            <button className={`${styles.getHelpButton} p-3 d-block d-md-block`}>
                 <Image src={"/icons/chatIcon.png"} alt='' width={75} height={75} className='img-fluid'></Image>
                 <p className='mb-0'>Get Help</p>
             </button>
